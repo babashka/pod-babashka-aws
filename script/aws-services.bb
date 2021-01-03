955c74981f9df4d241a1133a00874306fcf263c0
Jeroen van Dijk <jeroentjevandijk@gmail.com>
2021-01-03 14:58:25 +0100

    Validate services

--- script/aws-services.bb
+++ script/aws-services.bb
@@ -1,9 +1,14 @@
 #!/usr/bin/env bb
 
-(let [aws-libs (clojure.edn/read-string (slurp "https://raw.githubusercontent.com/cognitect-labs/aws-api/master/latest-releases.edn"))]
-  (spit "aws-libs.edn" 
-        (clojure.string/join "\n" 
-                             (map (fn [[k {:keys [mvn/version]}]]
-                                     (str k " " (pr-str {:mvn/version version}))) 
-                                  (sort-by (comp name key) aws-libs)))))
+(def latest-releases-url "https://raw.githubusercontent.com/cognitect-labs/aws-api/master/latest-releases.edn")
 
+(let [aws-libs (sort-by (comp name key)
+                        (-> (clojure.edn/read-string (slurp latest-releases-url))
+                            (dissoc 'com.cognitect.aws/api
+                                    'com.cognitect.aws/endpoints)))]
+  (spit "aws-libs.edn"
+        (clojure.string/join "\n"
+                             (map (fn [[k {:keys [mvn/version]}]]
+                                    (str k " " (pr-str {:mvn/version version})))
+                                  aws-libs)))
+  (spit "resources/aws-services.edn" (with-out-str (clojure.pprint/pprint (mapv (comp keyword name) (keys aws-libs))))))
--- src/pod/babashka/aws/impl/aws.clj
+++ src/pod/babashka/aws/impl/aws.clj
@@ -1,5 +1,7 @@
 (ns pod.babashka.aws.impl.aws
   (:require
+   [clojure.edn]
+   [clojure.java.io]
    [cognitect.aws.client.api :as aws]
    ;; these are dynamically loaded at runtime
    [cognitect.aws.http.cognitect]
@@ -16,18 +18,22 @@
 #_(defn get-credential-provider [config]
   (get @*credential-providers (get config ::credential-provider-id)))
 
+(def services (into (sorted-set) (clojure.edn/read-string (slurp (clojure.java.io/resource "aws-services.edn")))))
+
 (def *clients (atom {}))
 
 (defn get-client [config]
   (get @*clients (get config ::client-id)))
 
 (defn client [{:keys [api credentials-provider] :as config}]
-  (let [config (assoc config
-                      :http-client @http-client)
-        client (aws/client config)
-        client-id (java.util.UUID/randomUUID)]
-    (swap! *clients assoc client-id client)
-    {::client-id client-id}))
+  (if-not (contains? services api)
+    (throw (ex-info (str "api " api " not available") {:available services}))
+    (let [config (assoc config
+                        :http-client @http-client)
+          client (aws/client config)
+          client-id (java.util.UUID/randomUUID)]
+      (swap! *clients assoc client-id client)
+      {::client-id client-id})))
 
 (defn ops [client]
   (aws/ops (get-client client)))
