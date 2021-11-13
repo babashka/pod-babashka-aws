533e7af43114d172bcb848c0dbd4edd4b6299695
Michiel Borkent <michielborkent@gmail.com>
2021-11-13 10:12:58 +0100

    fix build

--- build.clj
+++ build.clj
@@ -1,3 +1,6 @@
+;; see https://ask.clojure.org/index.php/10905/control-transient-deps-that-compiled-assembled-into-uberjar?show=10913#c10913
+(require 'clojure.tools.deps.alpha.util.s3-transporter)
+
 (ns build
   (:require [clojure.string :as str]
             [clojure.tools.build.api :as b]))
